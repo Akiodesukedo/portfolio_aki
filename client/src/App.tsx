46a688725e1078149227536bd359a71186ef42b7
Akiodesukedo <abtai0227aki@gmail.com>
2025-04-19 10:49:02 -0700

    test: build tag just for test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Tag from './atoms/Tag'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -28,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Tag></Tag>
     </>
   )
 }
---
+++ client/src/atoms/Tag.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const Tag = () => {
+  const [tagName, setTagName] = useState("tag example")
+
+  return (
+    <div>
+      <button>
+        {tagName}
+      </button>
+    </div>
+  )
+}
+
+export default Tag
