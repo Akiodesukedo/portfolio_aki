f074e37bb683d75047d821bd30a3f71336466d8d
Akiodesukedo <abtai0227aki@gmail.com>
2025-05-07 13:36:21 -0700

    build: add scroll message

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -9,18 +9,25 @@ const Landing: React.FC<LandingProps> = ({}) => {
 
   const [title, setTitle] = useState<string>("Frontend Developer");
   const titleNum = useRef(0);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      titleNum.current += 1;
-      if (titleNum.current === titles.length) {
-        titleNum.current = 0;
-      }
+      setIsVisible(false);
 
-      setTitle(titles[titleNum.current])
-    }, 3000);
+      // Basically this setTimeout waits for the fade out before switching a title to another.
+      setTimeout(() => {
+        titleNum.current += 1;
+        if (titleNum.current === titles.length) {
+          titleNum.current = 0;
+        }
+  
+        setTitle(titles[titleNum.current])
+        setIsVisible(true);
+      }, 500)
+    }, 4000);
 
-    return () => clearInterval(interval); // Cleanup on unmount
+    return () => clearInterval(interval);
   }, [title]);
 
   const titles: string[] = [
@@ -41,19 +48,22 @@ const Landing: React.FC<LandingProps> = ({}) => {
         className="object-cover h-full"
       />
       <div className='absolute inset-0 bg-neutral-700 opacity-70'></div>
-      <div className='relative bottom-[300px]'>
-        <Marquee
-          speed={60}
-        >
-          <p className='text-[110px] font-medium text-white'>
-            Akifumi Hayashi&nbsp;
+      <div className='relative bottom-[280px] h-[280px] flex flex-col justify-between'>
+        <div>
+          <Marquee
+            speed={60}
+          >
+            <p className='text-[110px] font-medium text-white leading-[130px] '>
+              Akifumi Hayashi&nbsp;
+            </p>
+          </Marquee>
+          <p
+            className={`text-left text-white text-[26px] ml-[10px] transition-opacity duration-500 ease-in-out ${isVisible ? 'opacity-100': 'opacity-0'}`}
+          >
+            { title }
           </p>
-        </Marquee>
-        <p
-          className='text-left text-white text-[30px] font-medium'
-        >
-          { title }
-        </p>
+        </div>
+        <p className='text-right text-[30px] text-white font-medium mr-[10px] mb-[14px]'>Explore {"\u2193"}</p>
       </div>
 
     </div>
