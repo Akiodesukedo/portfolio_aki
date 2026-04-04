3864f2bd9eb3bca4fddec537a5f1bc9d42425bb2
Akiodesukedo <abtai0227aki@gmail.com>
2026-04-03 22:58:39 -0700

    fix: delete unecessary import

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import CtaBtn from "../atoms/CtaBtn"
-import LineDivider from "../atoms/LineDivider"
 
 type FooterProps = {
   imgUrl?: string
